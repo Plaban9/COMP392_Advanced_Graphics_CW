b171217dd42fe47d0ddbd91392bf877d7ce5a677
Plaban Biswas <plabanbiswas96@outlook.com>
2025-01-17 10:04:56 -0500

    - ADDED: Practice for Week - 2

---
+++ Classwork/Classwork/Week_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main()
+{
+	int i;
+
+	if (i = 1) // Checks value for i - anything other than 0 is true
+	{
+		std::cout << "true";
+	}
+	else
+	{
+		std::cout << "false";
+	}
+
+
+	int j = 10;
+
+	while (j--)
+	{
+		std::cout << j << std::endl;
+	}
+
+	int k = 10;
+
+	do
+	{
+		std::cout << k << std::endl;
+	} while (--k);
+
+	return 1;
+}
